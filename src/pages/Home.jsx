f6c7e264218d0ddcffaf4c772acbdeb4c18b6044
wincentrtz <wincentang3636@gmail.com>
2019-06-08 18:30:11 +0700

    Add graph

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,32 @@
 import React from "react";
 import Chart from "react-apexcharts";
 
-const Page1 = () => {
-  return <div>home</div>;
+const state = {
+  options: {
+    chart: {
+      id: "basic-bar"
+    },
+    xaxis: {
+      categories: [1991, 1992, 1993, 1994, 1995, 1996, 1997, 1998]
+    }
+  },
+  series: [
+    {
+      name: "series-1",
+      data: [30, 40, 45, 50, 49, 60, 70, 91]
+    }
+  ]
 };
 
-export default Page1;
+const Home = () => {
+  return (
+    <Chart
+      options={state.options}
+      series={state.series}
+      type="line"
+      width="1500"
+    />
+  );
+};
+
+export default Home;
